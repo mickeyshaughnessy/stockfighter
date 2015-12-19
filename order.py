e651e102ade4acdceabae09fa3ac415caaf607d2
Mickey Shaughnessy <mickeyshaughnessy@gmail.com>
2015-12-19 00:13:24 -0800

    fix up order script

--- order.py
+++ order.py
@@ -8,14 +8,14 @@ import requests
 import json
 
 headers = {"X-Starfighter-Authorization": "d4f6f80befe9cd49a65f470a1acea0bb227a104b"}
-payload = json.dumps({"orderType":"market","qty":100,"direction":"buy","account":"EMS12164524"})
+payload = json.dumps({"orderType":"market","qty":200,"direction":"buy","account":"EMS12164524"})
 print type(payload)
 
 
 ordered = 0
-while ordered < 100000:
+while ordered < 101000:
     r = requests.post("https://api.stockfighter.io/ob/api/venues/RKIMEX/stocks/BMOY/orders/", data=payload, headers=headers)
     ordered += int(r.json()['totalFilled'])
-    if ordered % 1000 = 0:
+    if ordered % 1000 == 0:
         print ordered
         print r.text
