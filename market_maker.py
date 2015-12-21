193bf24afb1639078ff8ec97587e77b46348db67
Mickey Shaughnessy <mickeyshaughnessy@gmail.com>
2015-12-21 02:08:44 -0800

    market maker not passing yet

---
+++ market_maker.py
@@ -0,0 +1,61 @@
+# this script deploys a market maker 
+"""
+curl https://api.stockfighter.io/ob/api/venues/AIOPEX/stocks/MEIA/quote --header "X-Starfighter-Authorization:d4f6f80befe9cd49a65f470a1acea0bb227a104b"
+curl -X POST -d '{"orderType":"market","qty":1,"direction":"buy","account":"WLS28175343"}' https://api.stockfighter.io/ob/api/venues/MRBTEX/stocks/OGV/orders --header "X-Starfighter-Authorization:d4f6f80befe9cd49a65f470a1acea0bb227a104b"
+"""
+
+import requests
+import json
+from gevent import spawn, sleep
+from multiprocessing import Process
+
+account = "BS76060286"
+key = "d4f6f80befe9cd49a65f470a1acea0bb227a104b"
+stock = "WAR"
+venue = "LGEHEX"
+url = 'https://api.stockfighter.io/ob/api/venues/%s/stocks/%s' % (venue, stock)
+headers = {"X-Starfighter-Authorization": "d4f6f80befe9cd49a65f470a1acea0bb227a104b"}
+payload_buymarket = json.dumps({"orderType":"market","qty":1,"direction":"buy","account":account})
+
+def quote():
+    r = requests.get(url+'/quote', headers=headers)
+    try:
+        bid, ask = r.json().get('ask'), r.json().get('bid')
+    except:
+        print r.text
+    return bid, ask
+ 
+def run_MarketMaker():
+    bid, ask = quote()
+    last_buy, last_sell, pos = bid, ask, 0
+    while 1:
+        bid, ask = quote()
+        if bid and ask and pos<400 and pos > -400:
+            if (bid > last_buy):
+                cost = max(last_buy, bid)
+                payload_selllimit = json.dumps({"orderType":"limit", "price":int(cost),"qty":10,"direction":"sell","account":account})
+                r1 = requests.post(url+'/orders', data=payload_selllimit, headers=headers)
+                pos -= int(r1.json()['qty'])
+                last_sell = int(r1.json()['price'])
+            
+            if (ask < last_sell):
+                cost = min(last_sell, ask)
+                payload_buylimit = json.dumps({"orderType":"limit","price":int(cost),"qty":10,"direction":"buy","account":account})
+                r2 = requests.post(url+'/orders', data=payload_buylimit, headers=headers)
+                pos += int(r2.json()['qty'])
+                last_buy = int(r2.json()['price'])
+        else:
+            print bid, ask
+        print pos 
+
+def listen_exec():
+    pass    
+
+if __name__ == '__main__':
+    def Gspawn():
+        G = [spawn(run_MarketMaker), spawn(listen_exec)]
+        [g.join() for g in G]
+                
+    P = [Process(target=Gspawn)]
+    [p.start() for p in P]
+    [p.join() for p in P]  
--- order.py
+++ order.py
@@ -8,7 +8,7 @@ import requests
 import json
 
 headers = {"X-Starfighter-Authorization": "d4f6f80befe9cd49a65f470a1acea0bb227a104b"}
-payload = json.dumps({"orderType":"market","qty":200,"direction":"buy","account":"EMS12164524"})
+payload = json.dumps({"orderType":"market","qty":100,"direction":"buy","account":"EMS12164524"})
 print type(payload)
 
 
